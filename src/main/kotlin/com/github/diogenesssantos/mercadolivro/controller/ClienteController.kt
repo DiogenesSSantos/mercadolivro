0dbd3dafaeb78951b83da7ca2e35aded6245489b
diogenes <diogenescontatoofficial@hotmail.com>
2025-07-01 20:16:22 -0300

    Modificações do ClienteController e Documentando OpenAPI

--- src/main/kotlin/com/github/diogenesssantos/mercadolivro/controller/ClienteController.kt
+++ src/main/kotlin/com/github/diogenesssantos/mercadolivro/controller/ClienteController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping(path = ["/clientes"])
+@RequestMapping(path = ["/cliente"])
 class ClienteController(val clienteService: ClienteService) : ClienteControlerAPIDocumentacao{
 
 
--- src/main/kotlin/com/github/diogenesssantos/mercadolivro/openapi/ConfigOpenAPI.kt
+++ src/main/kotlin/com/github/diogenesssantos/mercadolivro/openapi/ConfigOpenAPI.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -15,43 +16,40 @@ class ConfigOpenAPI {
     fun openAPI(): OpenAPI {
 
         return OpenAPI()
-            .info(Info().title("Kotlin").description("Alguma coisa"))
+            .info(
+                Info()
+                    .title("Mercado-Livro")
+                    .description("API PARA CADASTRO DE LIVRO")
+                    .version("1.0")
+                    .contact(
+                        Contact()
+                            .name("Diogenes da silva santos")
+                            .url("https://github.com/DiogenesSSantos")
+                            .email("diogenescontatoofficial@hotmail.com")
+                    ).license(License().name("MIT License").url("https://opensource.org/licenses/MIT"))
 
-
-
-
-//       return OpenAPI().apply {
-//
-//            info = Info().apply {
-//                title = "Integração WhatsApp - Agendamento de Saúde"
-//                description = """
-//                Esta API foi criada para integrar o canal de comunicação via WhatsApp
-//                com os sistemas de agendamento da Regulação de Saúde.
-//                A proposta é melhorar o fluxo de informações, permitindo que pacientes
-//                recebam notificações, lembretes e possam até mesmo confirmar ou
-//                reagendar compromissos diretamente pelo aplicativo.
-//                Essa abordagem visa reduzir faltas.
-//            """.trimIndent()
-//                version = "1.0.0"
-//
-//                contact = Contact().apply {
-//                    name = "Diogenes"
-//                    email = "diogenescontatoofficial@hotmail.com"
-//                    url = "https://github.com/DiogenesSSantos"
-//                }
-//
-//                license = License().apply {
-//                    name = "MIT License"
-//                    url = "https://opensource.org/licenses/MIT"
-//                }
-//            }
-//
-//            externalDocs = ExternalDocumentation().apply {
-//                description = "Documentação JavaDoc"
-//                url = "http://devdiogenes.shop/apidocs/index.html"
-//            }
-//        }
+            )
     }
 
+
+    @Bean
+    fun clienteOpenApi(): GroupedOpenApi {
+        return GroupedOpenApi
+            .builder()
+            .group("Cliente-Controller")
+            .pathsToMatch("/cliente/**")
+            .build()
+    }
+
+
+    @Bean
+    fun livroOpenApi() : GroupedOpenApi {
+        return GroupedOpenApi
+            .builder()
+            .group("Livro-Controller")
+            .pathsToMatch("/livro/**")
+            .build()
+
+    }
 
 }
